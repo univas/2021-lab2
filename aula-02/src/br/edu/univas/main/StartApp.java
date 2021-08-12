eed536c190ec0f95ad5e571f66e13449973ec5f4
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-08-12 20:32:37 -0300

    Add sample of methods

---
+++ aula-02/src/br/edu/univas/main/StartApp.java
@@ -0,0 +1,55 @@
+package br.edu.univas.main;
+
+public class StartApp {
+	
+	public static void main(String[] args) {
+		//executeSum();
+		//executeSub();
+		
+		print("Rodrigo", 34, 1500f, false);
+		
+		String pessoa = "João";
+		int xpto = 10;
+		float abc = 123.3f;
+		boolean qualquerCoisa = true;
+	}
+	
+	public static void executeSum() {
+		int numberOne = 10;
+		int numberTwo = 20;
+		int sum = numberOne + numberTwo;
+		
+		System.out.println("A soma dos números é: " + sum);
+		executeSub();
+	}
+	
+	public static void executeSub() {
+		int a = 100;
+		int b = 34;
+		int result = a - b;
+		System.out.println("A subtração dos números é: " + result);
+		
+		stop(10);
+		int x = 35;
+		stop(x);
+		
+		sum(20, 10);
+	}
+	
+	public static void stop(int pressao) {
+		System.out.println(pressao);
+	}
+	
+	public static void sum(int a, int b) {
+		int sum = a + b;
+		System.out.println("A soma deu: " + sum);
+	}
+	
+	public static void print(String nome, int idade, 
+			float salario, boolean estaChovendo) {
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Salário: " + salario);
+		System.out.println("Está Chovendo: " + estaChovendo);
+	}
+}
