2f2e05969ad1b94d912be3743228a8c5e04d86a5
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-09-11 06:07:43 -0300

    Add sample of creating new object

--- aula-05/src/br/edu/univas/main/StartApp.java
+++ aula-05/src/br/edu/univas/main/StartApp.java
@@ -15,20 +15,36 @@ public class StartApp {
 		
 		System.out.println("Novo nome: " + name);
 		
+		int[] notas = {16, 3, 10};
+		
+		changeNotas(notas);
+		
+		System.out.println("Notas: ");
+		System.out.println(notas[0]);
+		System.out.println(notas[1]);
+		System.out.println(notas[2]);
+		
+		int[] idades = {80, 81, 82};
+		changeIdade(idades);
+		
+		System.out.println("Idades: ");
+		System.out.println(idades[0]);
+		System.out.println(idades[1]);
+		System.out.println(idades[2]);
 	}
-	
 	public static void increment(int value) {
 		value++;
 	}
-	
 	public static void changeValue(String name) {
 		name = "NovoNomeParaVocê";
-	}
-	
+	}	
 	public static void changeNotas(int[] variaveis) {
 		variaveis[2] = 9;
 		variaveis[1] = 84;
 		variaveis[0] = 33;
+	}
+	public static void changeIdade(int[] idades) {
+		idades = new int[]{1, 2, 3};
 	}
 	
 }
